97e70a7d04d147c247d3d6febe7af6aa2824c56e
Amin <medamineharwel@outlook.com>
2022-07-06 15:34:54 +0100

    capacitor init

---
+++ waker-front/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.waker.front',
+  appName: 'waker-front',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
