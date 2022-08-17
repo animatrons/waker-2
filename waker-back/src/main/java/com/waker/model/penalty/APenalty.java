791e3f5334ea41aaabb908933f68b6e4b83aac48
Amin <medamineharwel@outlook.com>
2022-08-17 15:03:50 +0100

    abstract penalty class constructor

--- waker-back/src/main/java/com/waker/model/penalty/APenalty.java
+++ waker-back/src/main/java/com/waker/model/penalty/APenalty.java
@@ -1,8 +1,12 @@
 package com.waker.model.penalty;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public abstract class APenalty {
 
     private String signature;
